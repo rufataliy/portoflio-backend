c3d9027a3b477a7a2f2f47db51dbfc98773c4cfa
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-18 23:43:41 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3754e830f9944e014c4f253b1bc8f724'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9230f7a9-d3ca-4b08-b870-3590d9aafb83'
+};
